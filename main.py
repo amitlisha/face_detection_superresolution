4c390bf0609da46acc53af79daf1c1ea38409953
orberman1 <orberman5@gmail.com>
2023-10-09 00:10:21 +0300

    working with video and visualization works

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PIL import Image, ImageDraw
 import cv2
 import matplotlib.pyplot as plt
 import face_detection
+from utils import image_utils
 
 
 if __name__ == '__main__':
@@ -18,10 +19,18 @@ if __name__ == '__main__':
     #     end = (bb[2], bb[3])
     #     image_to_draw.rectangle([start, end], outline="red")
     # real_image.show()
-    img = Image.open("manyfaces.jpg")
-    video, bbs = face_detection.detection_pipeline([img])
-    x = 5
 
+    # img = Image.open("manyfaces.jpg")
+    # video, bbs = face_detection.detection_pipeline([img])
+    # x = 5
+
+    # video = image_utils.read_images_from_dir("examples")
+    video = image_utils.read_video("VID-20231007-WA0155.mp4")[150:200]
+    super_draws = []
+    for im in video:
+        bboxes, _, draws = face_detection.detection_pipeline([im])
+        super_draws.append(draws[0])
+    image_utils.save_images_to_dir(super_draws, dir_path="video frames with bbs")
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- yoloface-master/utils/image_utils.py
+++ yoloface-master/utils/image_utils.py
@@ -1,6 +1,34 @@
+import os
+
 import cv2
-from PIL import ImageDraw
+from PIL import ImageDraw, Image
 import numpy as np
+
+
+def read_video(video_path):
+    vidObj = cv2.VideoCapture(video_path)
+    success = 1
+    frames = []
+    while success:
+        success, image = vidObj.read()
+        if success:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            frames.append(Image.fromarray(image))
+    return frames
+
+
+def read_images_from_dir(dir_path):
+    images_names = os.listdir(dir_path)
+    images_ls = []
+    for image in images_names:
+        images_ls.append(Image.open(f"{dir_path}/{image}"))
+    return images_ls
+
+
+def save_images_to_dir(image_ls, dir_path="bb_examples"):
+    os.makedirs(dir_path, exist_ok=True)
+    for i, image in enumerate(image_ls):
+        image.save(f"{dir_path}/{i+1}.png")
 
 
 def resize_image(img, size: int):
@@ -18,14 +46,16 @@ def video_to_images_for_detection(video, size):
 def draw_bbs_on_video(video, bboxes):
     draw_frames = []
     for frame, bbs in zip(video, bboxes):
+        if type(frame) != Image:
+            frame = Image.fromarray(frame)
         draw_frame = draw_bbs_on_image(frame, bbs)
-        draw_frame.append(draw_frames)
+        draw_frames.append(draw_frame)
     return draw_frames
 
 
 def draw_bbs_on_image(img, bbs: list):
     image_to_draw = ImageDraw.Draw(img)
-    for bb in bbs[0]:
+    for bb in bbs:
         start = (bb[0], bb[1])
         end = (bb[2], bb[3])
         image_to_draw.rectangle([start, end], outline="red")
