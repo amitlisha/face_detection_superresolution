e71c8696e7cb6f2cfb5247521f8a3521db06b41d
orberman1 <orberman5@gmail.com>
2023-10-10 09:37:53 +0300

    finished documentation and code cleaning

--- argparser.py
+++ argparser.py
@@ -5,7 +5,8 @@ args.add_argument("--detection_batch_size", type=int, default=20, help="number o
                                                                        "-1 means all the video in one batch")
 args.add_argument("--min-face", type=int, default=10, help="minimum possible size of faces in image")
 args.add_argument("--visualize-bbs", default=True, action="store_true")
-args.add_argument("--scale", type=int, help="Super resolution model scale to use", default=2)
+args.add_argument("--scale", type=int, help="Super resolution model scale to use, "
+                                            "possible values are 2, 3, 4", default=2)
 args.add_argument("--save-path", type=str, help="path to where to save output", default="./results")
 
 args.add_argument("--device", type=str, default="cpu")
