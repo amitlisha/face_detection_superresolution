d9687fe25f85a1cdba1299d30aa2135c69dec5c3
dordor333 <dorhazan321@gmail.com>
2023-10-18 14:25:16 +0300

    Added skip frame distance to not take a lot of simillar or close images.

--- detection_and_super_resolution.py
+++ detection_and_super_resolution.py
@@ -7,35 +7,60 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 
 
+import os
+from PIL import Image
+
+def detect_faces_in_video(video):
+    """
+    Detect faces in the video.
+    """
+    if config['DEFAULT']['visualize-bbs']:
+        video, bbs, _ = face_detection.detection_pipeline(video)
+    else:
+        video, bbs = face_detection.detection_pipeline(video)
+    return video, bbs
+
+def load_super_resolution_model():
+    """
+    Load the super resolution model.
+    """
+    model = load_upscaler(config['DEFAULT']['device'], int(config['DEFAULT']['scale']))
+    model.eval()
+    return model
+
+def process_frame(frame, bb, model):
+    """
+    Process a single frame: upscale detected faces and return them.
+    """
+    upscaled_crops = upscale_crops(frame, bb, model, config['DEFAULT']['device'])
+    upscaled_images = []
+    for j, item in enumerate(upscaled_crops):
+        pil_img = Image.fromarray(item)
+        image_name = f"face_{j}_scale={config['DEFAULT']['scale']}.jpg"
+        upscaled_images.append((image_name, pil_img))
+    return upscaled_images
+
 def video_face_detection_and_super_resolution(video):
     """
-    This function executes the super resolution process for a given video. Crops of faces are extracted from each frame
-    and are than upsampled with super resolution. The function saves images of the crops in a chosen save path.
+    Execute super resolution process for a video. Extract and upscale face crops from each frame.
     @param video: a video as a list of PIL images
     @return: a list of PIL images of super resolution faces
     """
-    # if args.save_path is None:
-    #     path = args.video_path if args.video_path else args.image_path
-    #     args.save_path = f"results/{os.path.basename(path)}"
-    if config['DEFAULT']['visualize-bbs']:
-        video, bbs, _ = face_detection.detection_pipeline(video)
-        print(bbs)
-    else:
-        video, bbs = face_detection.detection_pipeline(video)
-    model = load_upscaler(config['DEFAULT']['device'], int(config['DEFAULT']['scale']))
-    model.eval()
-
-    # os.makedirs(f"{args.save_path}", exist_ok=True)
+    frame_skip_distance = int(config['DEFAULT']['frame-skip-distance'])
+    video, bbs = detect_faces_in_video(video)
+    model = load_super_resolution_model()
+    
     super_resolution_faces = []
+    skip_until = -1  # Index until which we should skip processing frames
+    
     for i, (frame, bb) in enumerate(zip(video, bbs)):
+        if i <= skip_until:
+            continue
         if len(bb) > 0:
-            upscaled_crops = upscale_crops(frame, bb, model, config['DEFAULT']['device'])
-            for j, item in enumerate(upscaled_crops):
-                pil_img = Image.fromarray(item)
-                # if args.save_path is not None:
-                image_name = f"frame_{i}_face_{j}_scale={config['DEFAULT']['scale']}.jpg"
+            upscaled_images = process_frame(frame, bb, model)
+            for image_name, pil_img in upscaled_images:
+                print(f"upscaled image created: frame_{i}_{image_name}")
+                super_resolution_faces.append((f"frame_{i}_{image_name}", pil_img))
+            skip_until = i + frame_skip_distance
 
-                print("upscaled image created: " + image_name)
-
-                super_resolution_faces.append((image_name, pil_img))
     return super_resolution_faces
